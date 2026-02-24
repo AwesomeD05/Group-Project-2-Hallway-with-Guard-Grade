606b7af158fb552177c983a7727ab4c03a57710f
nominal251 <jackhardy524@gmail.com>
2026-02-24 17:30:52 -0500

    add player controller

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float speed = 5.0f;
+    public float gravity = -9.8f;
+    Vector3 velocity;
+
+    public Transform mainCamera;
+
+    public float mouseSens;
+    private float xRotation;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        MouseLook();
+        Gravity();
+    }
+
+    void Movement()
+    {
+        float forwardInput = 0f;
+        float sideInput = 0f;
+
+        if (Keyboard.current.wKey.isPressed)
+            forwardInput += 1f;
+
+        if (Keyboard.current.sKey.isPressed)
+            forwardInput -= 1f;
+
+        if (Keyboard.current.aKey.isPressed)
+            sideInput -= 1f;
+
+        if (Keyboard.current.dKey.isPressed)
+            sideInput += 1f;
+
+        Vector3 move = (transform.right * sideInput + transform.forward * forwardInput).normalized;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    void MouseLook()
+    {
+        Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+
+        float mouseX = mouseDelta.x * mouseSens;
+        float mouseY = mouseDelta.y * mouseSens;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Rotate camera up/down
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        mainCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    void Gravity()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
